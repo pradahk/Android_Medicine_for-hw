1ef644885c27e6785eeb13accd91d231017ebee3
min-kyoung <shalsrud9@naver.com>
2020-12-09 13:28:50 +0900

    final

--- app/src/main/java/com/example/androidlogin/FindpwActivity.java
+++ app/src/main/java/com/example/androidlogin/FindpwActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,15 +16,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class FindpwActivity extends AppCompatActivity {
 
     // 파이어베이스 인증 객체 생성
-    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
-    // 작성한 이메일 값을 저장할 객체 생성
+    // 작성한 이메일 값과 이름을 저장할 객체 생성
     private EditText editTextEmail;
+    private EditText editTextName;
+
     private String sendemail = "";
+    private String sendname = "";
+
+    // 회원의 이메일을 보여줄 textview 객체 생성
+    private TextView textshowpw;
+    private TextView textshowtext;
+
+    // 로그인 화면으로 돌아가기 위한 버튼 객체 생성
+    private Button btngotologin;
 
     // 사용자에게 실시간 진행상태를 알려주는 ProgressDialog 객체 생성
     private ProgressDialog progressDialog;
@@ -39,14 +54,31 @@ public class FindpwActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_findpw);
 
-        // 파이어베이스 인증 객체 선언
-        firebaseAuth = FirebaseAuth.getInstance();
-
         // id가 write_email인 editText에 대한 메서드 저장
         editTextEmail = findViewById(R.id.write_email);
+        // id가 write_phone인 editText에 대한 메서드 저장
+        editTextName = findViewById(R.id.write_name);
+        // id가 tv_showid인 textview에 대한 메서드 저장
+        textshowpw = findViewById(R.id.tv_showpw);
+        // id가 tv_showtext인 textview에 대한 메서드 저장
+        textshowtext = findViewById(R.id.tv_showtext);
+
+        // 로그인 화면으로 돌아가는 버튼에 대한 메서드 저장
+        btngotologin = findViewById(R.id.btn_gotologin);
 
         // progressDialog 객체 선언
         progressDialog = new ProgressDialog(this);
+
+        btngotologin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //로그인으로 돌아가기 버튼을 누르면 MainActivity로 이동
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
@@ -54,11 +86,10 @@ public class FindpwActivity extends AppCompatActivity {
     {
         // editText에 작성한 내용을 String으로 변환하여 객체에 저장
         sendemail = editTextEmail.getText().toString();
+        sendname = editTextName.getText().toString();
 
-        // 이메일 유효성 검사 후 findpw 메서드 실행
-        if(isValidEmail()){
-            findpw(sendemail);
-        }
+       // findpw 메서드 실행
+        findpw(sendemail, sendname);
     }
 
     // 이메일 유효성 검사
@@ -74,34 +105,42 @@ public class FindpwActivity extends AppCompatActivity {
         }
     }
 
-    // 비밀번호를 찾기 위한 findpw 메서드
-    private void findpw(String sendemail) {
-        // 프로그래스 다이얼로그 생성하여 보여줌
+    // 아이디 찾기 메서드
+    private void findpw(final String sendname, final String sendphone) {
+        // 프로그레스 디이얼로그 생성하여 보여줌
         progressDialog.setMessage("처리중입니다. 잠시 기다려 주세요...");
         progressDialog.show();
-        //비밀번호 재설정 이메일 보내기
-        firebaseAuth.sendPasswordResetEmail(sendemail)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
+        // 파이어스토어 가져옴
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        // 파이어스토어의 collection 경로를 "users"로 생성
+        firebaseFirestore.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        // 파이어스토어의 "users"에서 정보를 가져오는 것이 성공하면
                         if (task.isSuccessful()) {
-                            // 이메일이 정상적으로 전송됐으면 "이메일을 발송하였습니다." 토스트를 보여줌
-                            Toast.makeText(FindpwActivity.this, R.string.emailsending, Toast.LENGTH_LONG).show();
-                            // 버튼을 누르면 메인화면으로 이동
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(intent);
-                            finish();
-                            // 이메일을 정상적으로 보낸 후 로그인 화면으로 이동
-                            //startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        } else {
-                            // 이메일이 정상적으로 전송되지 않았다면 "이메일 발송에 실패하였습니다." 토스트를 보여줌
-                            Toast.makeText(FindpwActivity.this, R.string.emailfail, Toast.LENGTH_LONG).show();
+                            for(QueryDocumentSnapshot document : task.getResult()) {
+                                // doument의 id와 입력한 이름이 같고 저장된 휴대폰번호와 입력한 휴대폰 번호가 같으면 입력한 이름의 저장된 이메일을 보여줌
+                                if (sendname.equals(document.getData().get("email")) && sendphone.equals(document.getData().get("name"))) {
+                                    textshowtext.setText("회원님의 비밀번호은 다음과 같습니다.");
+                                    textshowpw.setText(document.getData().get("password").toString());
+                                    // 로그인 화면으로 가는 버튼을 VISIBLE 처리하여 보여줌
+                                    btngotologin.setVisibility(View.VISIBLE);
+                                    break;
+                                }
+                                // 입력한 정보와 파이어베이스에 저장된 정보가 다르면 일치하는 회원정보가 없다는 텍스트를 보여줌
+                                else {
+                                    textshowtext.setText("일치하는 회원정보가 없습니다.");
+                                    textshowpw.setText("");
+                                }
+                            }
                         }
                         // 프로그래스 다이얼로그 사라짐
                         progressDialog.dismiss();
                     }
                 });
+
     }
 }
 
--- app/src/main/java/com/example/androidlogin/RecomMainActivity.java
+++ app/src/main/java/com/example/androidlogin/RecomMainActivity.java
@@ -1,6 +1,13 @@
 package com.example.androidlogin;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,42 +16,78 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
 public class RecomMainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
-    ArrayList<FormDrug> list = null;
+
+    Button[] recomBtn = new Button[8];
+    Button result_recombtn;
+    private String recom_id;
+
+    private String chooserecom = null;
+    //TextView textrecom;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recom_list);
+        setContentView(R.layout.recom_activity_main);
 
+        // 홈으로 이동
+        ImageButton btn_home = findViewById(R.id.gohome);
+
+        // 홈 버튼 클릭이벤트
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼을 누르면 메인화면으로 이동
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+
+        //증상 버튼 이벤트 실행
+        settingRecombtn();
     }
 
-    //증상에 맞는 약 json파일에서 검색
-    public void recomJson() {
-        try {
-            InputStream is = getAssets().open("recom.json"); //assests파일에 저장된 druglist_final.json 파일 열기
-            byte[] buffer = new byte[is.available()];
-            is.read(buffer);
-            is.close();
-            String json = new String(buffer, "UTF-8");
+    //증상 버튼 이벤트
+    public void settingRecombtn(){
+        for(int i=0; i <recomBtn.length; i++){
+            recom_id = "recom_btn" + (i+1); //버튼 아이디값 저장
+            recomBtn[i] = findViewById(getResources().getIdentifier(recom_id, "id",getPackageName())); //버튼 초기화
+        }
 
-            JSONObject jsonObject = new JSONObject(json);
-            JSONArray jsonArray = jsonObject.getJSONArray("recomlist"); //json파일에서 의약품리스트의 배열명, jsonArray로 저장
+        for(Button buttonId : recomBtn){
+            buttonId.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    result_recombtn = findViewById(v.getId());
+                    //result_recombtn.setBackgroundResource(R.drawable.choose_btton); //해당아이디 버튼의 배경색을 바꿈
+                    //result_recombtn.setTextColor(Color.WHITE);
+                    chooserecom = result_recombtn.getText().toString(); //선택 증상을 저장
 
-            list = new ArrayList<>();
+                    //////여기서 for문으로 thiscolor랑 result.getText.toString()비교해서 배경색 다시 바꿔주기
+                    Log.e("다음 클릭 후 : ", chooserecom);
 
-            for (int i = 0; i < jsonArray.length(); i++) {
-                jsonObject = jsonArray.getJSONObject(i);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+                    Intent intent = new Intent(getApplicationContext(), RecomSearchActivity.class);
+                    intent.putExtra("chooserecom",chooserecom);
+
+                    Log.e("길이 : ", String.valueOf(chooserecom.length()));
+                    startActivity(intent);
+                }
+            });
         }
     }
+
 }
