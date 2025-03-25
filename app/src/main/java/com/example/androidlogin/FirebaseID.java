43762351c80fb951c4fec24e0d55ff47ec66db30
pradahk <80239777+pradahk@users.noreply.github.com>
2025-03-25 11:30:12 +0900

    Update FirebaseID.java

--- app/src/main/java/com/example/androidlogin/FirebaseID.java
+++ app/src/main/java/com/example/androidlogin/FirebaseID.java
@@ -1,8 +1,12 @@
+// Firebase 데이터베이스에서 사용할 키(필드명)를 정의하는 클래스
 package com.example.androidlogin;
 
 public class FirebaseID {
+    //static 변수를 선언하여 공통적으로 사용할 키 이름을 저장
     public static String user = "user";
     public static String email = "email";
     public static String name = "name";
+    //public static String으로 선언하여 클래스 이름을 통해 직접 접근 가능 (FirebaseID.user).
+    //Firebase에 데이터를 저장할 때 사용할 키 값을 미리 정의.
 
 }
